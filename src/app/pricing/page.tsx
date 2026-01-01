a9cb46812ddb2adcbc65db474956ed851272bea5
pota2dev <mahdihasan535@gmail.com>
2026-01-01 18:33:08 +0600

    Added "Pricing" page and a global "Navbar"

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,10 @@
+
+import { currentUser } from "@clerk/nextjs/server";
+import PricingView from "./view";
+
+export default async function PricingPage() {
+  const user = await currentUser();
+  const isPro = user?.publicMetadata?.plan === "pro";
+
+  return <PricingView isPro={isPro} />;
+}
